3a34998e0b2aed01e9ac270ca169cfa7133b17ec
lalalakolpachok <akolpakova91@gmail.com>
2018-08-30 20:45:31 +0300

    add error page and handler

---
+++ README.md
@@ -0,0 +1,6 @@
+# Домашнее задание #3 для курса Node.js в Loft School
+
+## Проект состоит из 3 страниц:
+* index.html
+* login.html
+* admin.html
--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
 
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  // render the error page
+  res.status(err.status || 500);
+  res.render('pages/error', {
+    message: err.message,
+    error: err,
+    status: err.status
+  });
+});
+
 const server = app.listen(process.env.PORT || 3000, function () {
   console.log('Сервер запущен на порте: ' + server.address().port);
 });
